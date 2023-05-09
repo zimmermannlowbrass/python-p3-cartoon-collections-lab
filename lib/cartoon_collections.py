f55521df57f42059f2da3ebff9d63534a8ba11d0
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-05-08 20:04:39 -0500

    completed labwork

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,20 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for x in range(len(dwarves)):
+        print(f"{x + 1}. {dwarves[x]}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(elements):
+    new_list = []
+    for el in elements:
+        new_list.append(el.title() + '!')
+    return new_list
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(elements):
+    for el in elements:
+        if len(el) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(elements):
+    for el in elements:
+        if el == 'cheddar':
+            return el
